fb6a112fe62298a95bf18c0f9d8f008c4592013f
olejodd10 <67640511+olejodd10@users.noreply.github.com>
2021-06-27 18:03:46 +0200

    Prepare for additional RunConfigs

--- src/run/interact.rs
+++ src/run/interact.rs
@@ -6,30 +6,31 @@ use super::RunConfig;
 pub fn interact_remote(input: &mut dyn BufRead, output: &mut dyn Write, run_config: &RunConfig) -> Result<(), Box<dyn std::error::Error>> {
     // https://doc.rust-lang.org/std/process/index.html
 
-    match run_config {
+    let mut input_vec = Vec::new();
+    input.read_to_end(&mut input_vec)?;
+    
+    let mut child = match run_config {
         RunConfig::Rust => {
-            let mut input_vec = Vec::new();
-            input.read_to_end(&mut input_vec)?;
-        
-            let mut child = Command::new("cargo")
+            Command::new("cargo")
                 .arg("run")
                 .stdin(Stdio::piped())
                 .stdout(Stdio::piped())
                 .spawn()
-                .expect("Failed to execute child");
-        
-            let mut input_pipe = child.stdin.take().expect("Failed to get stdin");
-        
-            std::thread::spawn(move || {
-                input_pipe.write_all(&input_vec).expect("Failed to write to stdin");
-            });
-        
-            let output_pipe = child
-                .wait_with_output()
-                .expect("Failed to wait on child");
-        
-            output.write_all(output_pipe.stdout.as_slice())?;
+                .expect("Failed to execute child")
         },
-    }
+    };
+
+    let mut input_pipe = child.stdin.take().expect("Failed to get stdin");
+
+    std::thread::spawn(move || {
+        input_pipe.write_all(&input_vec).expect("Failed to write to stdin");
+    });
+
+    let output_pipe = child
+        .wait_with_output()
+        .expect("Failed to wait on child");
+
+    output.write_all(output_pipe.stdout.as_slice())?;
+
     Ok(())
 }
